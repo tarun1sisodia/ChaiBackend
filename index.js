65c5908f4a87a18629882f5329094b946eb2f2a1
tarun1sisodia <coccoder999@gmail.com>
2025-10-01 13:46:16 +0530

    Create First Server with 4 Files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+//we are making an object for express framework to use.
+const app = express();
+
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send("Root");
+});
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please Go to Hitesh Sir backend playlist</h2>');
+});
+
+app.listen(PORT, () => {
+    // print();
+    // setInterval(() => { print(); }, 2000);
+    console.log(`Server Listening on ${PORT}`);
+});
+
+app.get('/x', (req, res) => {
+    res.send('Congrats You Are Admin at X');
+});
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>CHai AUr COde</h2>');
+});
+/* function print() {
+    console.log("Nothing Found:");
+} */
