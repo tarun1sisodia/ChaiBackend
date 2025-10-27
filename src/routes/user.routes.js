1b7f79f7801a744f8e5f2b640f31881feb80f116
tarun1sisodia <coccoder999@gmail.com>
2025-10-27 20:28:20 +0530

    added my own response and health get req

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 
 const router = Router();
 
 // Setting up router apis
 // /register will call the post method(Register user) and what and how much to upload on localServer
 router.route("/register").post(
-    // what to upload and how much
+  // what to upload and how much
   upload.fields([
     { name: "avatar", maxCount: 1 },
     { name: "coverImage", maxCount: 1 },
@@ -19,4 +20,8 @@ router.route("/login").get(loginUser);
 console.log(router);
 // console.log(upload.fields());
 
+// Health API
+router
+  .route("/health")
+  .get((req, res) => res.send(new ApiResponse(200, `Server is Healthy`)));
 export default router;
