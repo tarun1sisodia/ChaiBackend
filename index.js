f8a71fa1248a033f73946499b3d29ae43e5853c2
tarun1sisodia <coccoder999@gmail.com>
2026-06-18 09:44:20 +0530

    feat: route for fetch users details

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import configDotenv from "dotenv";
 configDotenv.config();
 
+
 import express from "express";
 const app = express();
 const port = 5000;
@@ -25,8 +26,42 @@ app.post('/register/users', (req, res) => {
     res.send('Create something... 201')
 });
 
-app.put('/user', (req, res) => {
-    res.send('Replace entire detailss.... ')
+app.get('/github/me', (req, res) => {
+    res.json({
+        "login": "tarun1sisodia",
+        "id": 128500527,
+        "node_id": "U_kgDOB6jDLw",
+        "avatar_url": "https://avatars.githubusercontent.com/u/128500527?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/tarun1sisodia",
+        "html_url": "https://github.com/tarun1sisodia",
+        "followers_url": "https://api.github.com/users/tarun1sisodia/followers",
+        "following_url": "https://api.github.com/users/tarun1sisodia/following{/other_user}",
+        "gists_url": "https://api.github.com/users/tarun1sisodia/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/tarun1sisodia/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/tarun1sisodia/subscriptions",
+        "organizations_url": "https://api.github.com/users/tarun1sisodia/orgs",
+        "repos_url": "https://api.github.com/users/tarun1sisodia/repos",
+        "events_url": "https://api.github.com/users/tarun1sisodia/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/tarun1sisodia/received_events",
+        "type": "User",
+        "user_view_type": "public",
+        "site_admin": false,
+        "name": "Tarun Sisodia",
+        "company": "tarun1sisodia@gmail.com",
+        "blog": "https://tarun-sisodia-nup9tne.gamma.site/",
+        "location": "India",
+        "email": null,
+        "hireable": true,
+        "bio": "MCA Cybersecurity. \r\nCurrently,on APP & Backend Dev.. \r\nAutomation using AI agents",
+        "twitter_username": "tarun1sisodia",
+        "public_repos": 28,
+        "public_gists": 0,
+        "followers": 14,
+        "following": 25,
+        "created_at": "2023-03-21T15:03:02Z",
+        "updated_at": "2026-06-08T13:42:05Z"
+    })
 });
 
 app.delete('/users/me', (req, res) => {
